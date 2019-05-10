e65327a0d249530b58df7b2a69f44ba686fa13d3
arkanto99 <pablo.rodriguez.fernandez0@rai.usc.es>
2019-05-10 15:56:28 +0200

    Avances CholeskyTrid2

--- practica3/CholeskyTri/choltrid_ppal.f95
+++ practica3/CholeskyTri/choltrid_ppal.f95
@@ -25,44 +25,53 @@ read*,n
 allocate(as(n-1), ad(n),x(n),y(n-1),b(n), u(n), w(n), r(n))
 
 !Lectura de datos
-print*,'Introduza a diagonal principal'
-read*,ad
-print*,'Introduza a diagonal secundaria'
-read*,as
+print*,'Introduza os elementos da diagonal principal '
+read*,ad(1:n)
+print*,'Introduza os elementos da diagonal secundaria '
+read*,as(1:n-1)
+print*,'Introduza os elementos do termo independente '
+read*,b(1:n)
 print*,'Diagonal principal'
 print formato4, ad
 print*,'Diagonal secundaria'
 print formato4, as
+print*,'Termo independente'
+print formato4,b
 
-!Proceso de eliminacion
+!!!!!!!!!!!!!!!!!!!!!!!!!!!11Proceso de eliminacion(verificado)!!!!!!!!!!!!!!!!!!1
 x(1)=sqrt(ad(1))
 do i=2,n
 	y(i-1)=as(i-1)/x(i-1)
-	x(i)=sqrt(a(i)-y(i-1)**2)
+	x(i)=sqrt(ad(i)-y(i-1)**2)
 end do
 
-!Descenso
-call sistl(a,b,w)
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!DESCENSO, NO FUNCIONA!!!!!!!!!!!!!!!!!!!!!!!!!
+do i=2,n
+	b(i)=b(i)-y(i)*b(i-1)
+end do
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!REMONTE, NO FUNCIONA!!!!!!!!!!!!!!!!!!!!!!!!!!
+b(n)=b(n)/x(n)
+do i=n-1,1,-1
+	b(i)=(b(i)-y(i)*b(i+1))/x(i)
+end do
 
-a=TRANSPOSE(a) 
-!Remonte
-call sistu(a,w,u)
-
-call residuo(aa,b,u,r)
+!call residuo(aa,b,u,r)
 
 print*,' '
-print*,'O resultado u ,empregando a factorizacion de Cholesky, e:'
-print formato10,u
+print*,'O resultado u ,empregando a factorizacion de Cholesky Tridiagonal, e:'
+print formato10,b
 print*,' '
-print*,'O determinante de A ten o seguinte valor: '
-print*,deter
+print*, 'A diagonal principal da factorizacion de Cholesky e:'
+print formato10,x
 print*,' '
-print*,'El residuo r=Au-b es:'
-print formato4,r
-print*,' '
-print*,'La norma del residuo es :'
-print formato4,sqrt(dot_product(r,r))
+print*,'A subdiagonal inferior (A superior e 0) da factorizacion de Cholesky e:'
+print formato10,y
+!print*,'El residuo r=Au-b es:'
+!print formato4,r
+!print*,' '
+!print*,'La norma del residuo es :'
+!print formato4,sqrt(dot_product(r,r))
 
-deallocate(a,aa,b,w,u,r)
+!deallocate(a,aa,b,w,u,r)
 
 end program
